0c4d26cbd59ffec33a39e72cd457c45dbc489cd3
KingHyperion <anirudhkrushnakumar@gmail.com>
2026-03-13 21:33:20 -0400

    Fixed class selection error

--- main_game.py
+++ main_game.py
@@ -10,8 +10,8 @@ level_p = 1
 
 valid_input1 = False
 while not valid_input1:
-    player_class_selection = input("Please choose one of the classes below:\n A) Warrior: A martial fighter, focusing on strength.\n B) Mage: An arcane user of magic, focusing on intelligence but with low health.\n C) Thief: A cunning rogue, focusing on stealth to sneak around.\n")
-    if player_class_selection == "A" or "a":
+    player_class_selection = input("Please choose one of the classes below:\n A) Warrior: A martial fighter, focusing on strength.\n B) Mage: An arcane user of magic, focusing on intelligence but with low health.\n C) Thief: A cunning rogue, focusing on stealth to sneak around.\n").strip().upper()
+    if player_class_selection == "A":
         player_class = "Warrior"
         health_p = 10 * level_p + 1
         strength_p = 15
@@ -20,7 +20,7 @@ while not valid_input1:
         intelligence_p = 8
         valid_input1 = True
 
-    elif player_class_selection == "B" or "b":
+    elif player_class_selection == "B":
         player_class = "Mage"
         health_p = 4 * level_p + 1
         strength_p = 8
@@ -29,7 +29,7 @@ while not valid_input1:
         intelligence_p = 15
         valid_input1 = True
 
-    elif player_class_selection == "C" or "c":
+    elif player_class_selection == "C":
         player_class = "Thief"
         health_p = 8 * level_p + 1
         strength_p = 12
@@ -43,4 +43,4 @@ while not valid_input1:
 
 print("You have selected: " + player_class)
 name_p = input("Please enter a name for your character: ")
-print("Player description:\n Class: " + player_class + "\n Health: " + str(health_p) + "\n Strength: " + str(strength_p) + "\n Dexterity: " + str(dexterity_p) + "\n Constitution: " + str(constitution_p) + "\n Intelligence: " + str(intelligence_p) + "\n ========================================")
+print("Player description:\n Name: " + name_p + "\n Class: " + player_class + "\n Health: " + str(health_p) + "\n Strength: " + str(strength_p) + "\n Dexterity: " + str(dexterity_p) + "\n Constitution: " + str(constitution_p) + "\n Intelligence: " + str(intelligence_p) + "\n ========================================")
